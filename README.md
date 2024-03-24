5c47b72a49585c8a52e84491508ef9533de0e688
yus-ko <yus-ko>
2024-03-25 04:43:03 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # controller_tutorial
+
+# ビルド方法
+ 
+```bash
+cd ~/catkin_ws/src
+git clone https://github.com/yus-ko/potbot
+git clone https://github.com/yus-ko/controller_tutorial
+```
+```bash
+cd ~/catkin_ws
+catkin build controller_tutorial
+```
+
+# 起動方法
+
+```bash
+roslaunch controller_tutorial controller_tutorial.launch
+...
--- src/controller_tutorial.cpp
+++ src/controller_tutorial.cpp
@@ -1,188 +1,129 @@
 #include <ros/ros.h>
-#include <potbot_lib/PotentialField.h>
-#include <potbot_lib/PathPlanner.h>
-#include <potbot_lib/Utility.h>
-// #include <sensor_msgs/PointCloud2.h>
-// #include <nav_msgs/Path.h>
-// #include <geometry_msgs/PoseWithCovarianceStamped.h>
-// #include <geometry_msgs/PoseStamped.h>
-// #include <geometry_msgs/PointStamped.h>
-// #include <dynamic_reconfigure/server.h>
-// #include <apf_pathplanner_tutorial/apf_pathplanner_tutorialConfig.h>
+#include <potbot_lib/DiffDriveController.h>
+#include <dynamic_reconfigure/server.h>
+#include <controller_tutorial/controller_tutorialConfig.h>
+#include <geometry_msgs/PoseArray.h>
+#include <random>
 
-// geometry_msgs::PoseWithCovarianceStamped g_robot;
-// geometry_msgs::PoseStamped g_goal;
-// std::vector<geometry_msgs::PointStamped> g_obstacles;
+std::vector<potbot_lib::Controller::DiffDriveController> g_robot;
 
-// double g_potential_field_rows					= 240;
-// double g_potential_field_cols					= 240;
-// double g_potential_field_resolution				= 0.05;
-// double g_weight_attraction_field				= 0.1;
-// double g_weight_repulsion_field					= 0.1;
-// double g_distance_threshold_repulsion_field		= 0.3;
-// double g_max_path_length						= 6.0;
-// size_t g_path_search_range						= 1;
-// std::string g_potential_field_filter_mode		= "and";
-// std::vector<size_t> g_potential_field_filter_terms;
+void inipose_callback(const geometry_msgs::PoseWithCovarianceStamped& msg)
+{
+	nav_msgs::Odometry ini_pose;
+	ini_pose.header.frame_id = "map" ;
+	ini_pose.pose = msg.pose;
+	for (auto& robo : g_robot) robo.set_msg(ini_pose);
+}
 
-// void inipose_callback(const geometry_msgs::PoseWithCovarianceStamped& msg)
-// {
-//     g_robot = msg;
-// }
+void goal_callback(const geometry_msgs::PoseStamped& msg)
+{
+	g_robot[0].set_target(	msg.pose.position.x,
+							msg.pose.position.y,
+							potbot_lib::utility::get_Yaw(msg.pose.orientation));
+}
 
-// void goal_callback(const geometry_msgs::PoseStamped& msg)
-// {
-//     g_goal = msg;
-// }
+void param_callback(const controller_tutorial::controller_tutorialConfig& param, uint32_t level)
+{
+	g_robot[0].set_gain(	param.gain_p,
+							param.gain_i,
+							param.gain_d);
 
-// void point_callback(const geometry_msgs::PointStamped& msg)
-// {
-//     g_obstacles.push_back(msg);
-// }
+	g_robot[0].set_margin(	param.stop_margin_angle,
+							param.stop_margin_distance);
 
-// void param_callback(const apf_pathplanner_tutorial::apf_pathplanner_tutorialConfig& param, uint32_t level)
-// {
-// 	g_potential_field_rows					= param.potential_field_rows;
-// 	g_potential_field_cols					= param.potential_field_cols;
-// 	g_potential_field_resolution			= param.potential_field_resolution;
-// 	g_weight_attraction_field				= param.weight_attraction_field;
-// 	g_weight_repulsion_field				= param.weight_repulsion_field;
-// 	g_distance_threshold_repulsion_field	= param.distance_threshold_repulsion_field;
-// 	g_max_path_length						= param.max_path_length;
-// 	g_path_search_range						= param.path_search_range;
-
-// 	g_potential_field_filter_mode			= param.potential_field_grid_filter;
-// 	g_potential_field_filter_terms.clear();
-// 	if (param.goal)							g_potential_field_filter_terms.push_back(potbot_lib::Potential::GridInfo::IS_GOAL);
-// 	if (param.robot)						g_potential_field_filter_terms.push_back(potbot_lib::Potential::GridInfo::IS_ROBOT);
-// 	if (param.obstacle)						g_potential_field_filter_terms.push_back(potbot_lib::Potential::GridInfo::IS_OBSTACLE);
-// 	if (param.repulsion_inside)				g_potential_field_filter_terms.push_back(potbot_lib::Potential::GridInfo::IS_REPULSION_FIELD_INSIDE);
-// 	if (param.repulsion_edge)				g_potential_field_filter_terms.push_back(potbot_lib::Potential::GridInfo::IS_REPULSION_FIELD_EDGE);
-// 	if (param.path)							g_potential_field_filter_terms.push_back(potbot_lib::Potential::GridInfo::IS_PLANNED_PATH);
-// 	if (param.around_goal)					g_potential_field_filter_terms.push_back(potbot_lib::Potential::GridInfo::IS_AROUND_GOAL);
-// 	if (param.local_minimum)				g_potential_field_filter_terms.push_back(potbot_lib::Potential::GridInfo::IS_LOCAL_MINIMUM);
-// }
-
-// int main(int argc,char **argv){
-// 	ros::init(argc,argv,"apf_pathplanner_tutorial");
-
-// 	ros::NodeHandle nh;
-
-// 	ros::Publisher pub_attraction_field		= nh.advertise<sensor_msgs::PointCloud2>("field/attraction", 1);
-// 	ros::Publisher pub_repulsion_field		= nh.advertise<sensor_msgs::PointCloud2>("field/repulsion", 1);
-// 	ros::Publisher pub_potential_field		= nh.advertise<sensor_msgs::PointCloud2>("field/potential", 1);
-// 	ros::Publisher pub_filtered_field		= nh.advertise<sensor_msgs::PointCloud2>("field/filtered", 1);
-// 	ros::Publisher pub_path_raw				= nh.advertise<nav_msgs::Path>("path/raw", 1);
-// 	ros::Publisher pub_path_interpolated	= nh.advertise<nav_msgs::Path>("path/interpolated", 1);
-
-// 	ros::Subscriber sub_inipose				= nh.subscribe("initialpose",1,inipose_callback);
-// 	ros::Subscriber sub_goal				= nh.subscribe("move_base_simple/goal",1,goal_callback);
-// 	ros::Subscriber sub_point				= nh.subscribe("clicked_point",1,point_callback);
-
-// 	dynamic_reconfigure::Server<apf_pathplanner_tutorial::apf_pathplanner_tutorialConfig> server;
-// 	dynamic_reconfigure::Server<apf_pathplanner_tutorial::apf_pathplanner_tutorialConfig>::CallbackType f;
-// 	f = boost::bind(param_callback, _1, _2);
-// 	server.setCallback(f);
-
-// 	while (ros::ok())
-// 	{
-// 		potbot_lib::utility::Timer timer;
-//         timer.start("1 loop");
-
-//         potbot_lib::PathPlanner::APFPathPlanner apf(
-// 							g_potential_field_rows,					//ポテンシャル場の幅(x軸方向) [m]
-// 							g_potential_field_cols,					//ポテンシャル場の高さ(y軸方向) [m]
-// 							g_potential_field_resolution,			//ポテンシャル場グリッド1辺の長さ [m]
-// 							g_weight_attraction_field,				//ポテンシャル場における引力場の重み
-// 							g_weight_repulsion_field,				//ポテンシャル場における斥力場の重み
-// 							g_distance_threshold_repulsion_field	//斥力場を場を作る距離の閾値 [m]
-// 							);
-//         apf.set_goal(	g_goal.pose.position.x, 		g_goal.pose.position.y);
-//         apf.set_robot(	g_robot.pose.pose.position.x, 	g_robot.pose.pose.position.y);
-		
-// 		for (auto obs : g_obstacles)
-// 		{
-// 			apf.set_obstacle(obs.point.x,				obs.point.y);
-// 		}
-
-// 		// for (double inc = -3; inc <=3; inc += 0.05)
-// 		// {
-// 		// 	double y = inc;
-// 		// 	double x = 3;
-// 		// 	apf.set_obstacle(x,y);
-// 		// }
-
-// 		timer.start("potential");
-//         apf.create_potential_field();
-// 		timer.stop("potential");
-
-// 		std::vector<std::vector<double>> path_raw, path_interpolated;
-// 		double init_yaw = potbot_lib::utility::get_Yaw(g_robot.pose.pose.orientation);
-// 		if (isnan(init_yaw)) init_yaw = 0;
-
-// 		timer.start("path");
-// 		apf.create_path(path_raw, init_yaw, g_max_path_length, g_path_search_range);
-// 		apf.bezier(path_raw, path_interpolated);
-// 		timer.stop("path");
-
-// 		timer.print_time();
-
-// 		potbot_lib::Potential::Field attraction_field, repulsion_field, potential_field, filtered_field;
-// 		apf.get_attraction_field(attraction_field);
-// 		apf.get_repulsion_field(repulsion_field);
-// 		apf.get_potential_field(potential_field);
-// 		// potential_field.info_filter(filtered_field, {potbot_lib::Potential::GridInfo::IS_PLANNED_PATH, potbot_lib::Potential::GridInfo::IS_REPULSION_FIELD_EDGE},"and");
-// 		potential_field.info_filter(filtered_field, g_potential_field_filter_terms, g_potential_field_filter_mode);
-
-// 		nav_msgs::Path path_msg_raw, path_msg_interpolated;
-// 		for (auto point : path_raw)
-// 		{
-// 			geometry_msgs::PoseStamped pose_msg;
-// 			pose_msg.pose.position.x = point[0];
-// 			pose_msg.pose.position.y = point[1];
-// 			path_msg_raw.poses.push_back(pose_msg);
-// 		}
-// 		for (auto point : path_interpolated)
-// 		{
-// 			geometry_msgs::PoseStamped pose_msg;
-// 			pose_msg.pose.position.x = point[0];
-// 			pose_msg.pose.position.y = point[1];
-// 			path_msg_interpolated.poses.push_back(pose_msg);
-// 		}
-
-//         sensor_msgs::PointCloud2 attraction_field_msg, repulsion_field_msg, potential_field_msg, filtered_field_msg;
-//         attraction_field.to_pcl2(attraction_field_msg);
-//         repulsion_field.to_pcl2(repulsion_field_msg);
-//         potential_field.to_pcl2(potential_field_msg);
-// 		filtered_field.to_pcl2(filtered_field_msg);
-
-//         std_msgs::Header header_apf;
-//         header_apf.frame_id				= "map";
-//         header_apf.stamp				= ros::Time::now();
-//         attraction_field_msg.header		= header_apf;
-//         repulsion_field_msg.header		= header_apf;
-//         potential_field_msg.header		= header_apf;
-// 		filtered_field_msg.header		= header_apf;
-// 		path_msg_raw.header				= header_apf;
-// 		path_msg_interpolated.header	= header_apf;
-
-//         pub_attraction_field.publish(attraction_field_msg);
-//         pub_repulsion_field.publish(repulsion_field_msg);
-//         pub_potential_field.publish(potential_field_msg);
-// 		pub_filtered_field.publish(filtered_field_msg);
-// 		pub_path_raw.publish(path_msg_raw);
-// 		pub_path_interpolated.publish(path_msg_interpolated);
-
-// 		ros::spinOnce();
-
-// 		timer.stop("1 loop");
-// 		timer.print_time("1 loop");
-		
-// 	}
-
-// 	return 0;
-// }
+	g_robot[0].set_limit(	param.max_linear_velocity,
+							param.max_angular_velocity);
+}
 
 int main(int argc,char **argv){
+	ros::init(argc,argv,"controller_tutorial");
+
+	ros::NodeHandle n("~");
+
+	double control_frequency = 50.0;
+	int particle_num = 100;
+	double norm_noise_mean_linear_velocity = 0;
+	double norm_noise_variance_linear_velocity = 0.1;
+	double norm_noise_mean_angular_velocity = 0;
+	double norm_noise_variance_angular_velocity = 0.1;
+	
+	n.getParam("control_frequency", control_frequency);
+	n.getParam("particle_num", particle_num);
+	n.getParam("norm_noise_mean_linear_velocity", norm_noise_mean_linear_velocity);
+	n.getParam("norm_noise_variance_linear_velocity", norm_noise_variance_linear_velocity);
+	n.getParam("norm_noise_mean_angular_velocity", norm_noise_mean_angular_velocity);
+	n.getParam("norm_noise_variance_angular_velocity", norm_noise_variance_angular_velocity);
+
+	g_robot.resize(particle_num+1);
+
+	ros::NodeHandle nh;
+
+	ros::Publisher pub_odom					= nh.advertise<nav_msgs::Odometry>("odom", 1);
+	ros::Publisher pub_particles			= nh.advertise<geometry_msgs::PoseArray>("particles", 1);
+
+	ros::Subscriber sub_inipose				= nh.subscribe("initialpose",1,inipose_callback);
+	ros::Subscriber sub_goal				= nh.subscribe("move_base_simple/goal",1,goal_callback);
+
+	dynamic_reconfigure::Server<controller_tutorial::controller_tutorialConfig> server;
+	dynamic_reconfigure::Server<controller_tutorial::controller_tutorialConfig>::CallbackType f;
+	f = boost::bind(param_callback, _1, _2);
+	server.setCallback(f);
+	
+	ros::Rate rate(control_frequency);
+
+	nav_msgs::Odometry robot_pose;
+	robot_pose.header.frame_id = "map";
+	robot_pose.pose.pose = potbot_lib::utility::get_Pose(0,0,0,0,0,0);	//ロボット初期位置 x y z r p y
+
+	geometry_msgs::PoseArray particles_msg;
+	particles_msg.header.frame_id = robot_pose.header.frame_id;
+	for (size_t i = 0; i < particle_num; i++) particles_msg.poses.push_back(robot_pose.pose.pose);
+
+	for (auto& robo : g_robot)
+	{
+		robo.deltatime = 1.0/control_frequency;;
+		robo.set_msg(robot_pose);
+	}
+
+	std::random_device rd;
+    std::default_random_engine generator(rd());
+    std::normal_distribution<double> distribution_linear_velocity(norm_noise_mean_linear_velocity, sqrt(norm_noise_variance_linear_velocity));
+	std::normal_distribution<double> distribution_angular_velocity(norm_noise_mean_angular_velocity, sqrt(norm_noise_variance_angular_velocity));
+
+	while (ros::ok())
+	{
+
+		g_robot[0].pid_control();
+		g_robot[0].update();
+		
+		g_robot[0].to_msg(robot_pose);
+
+		particles_msg.header = robot_pose.header;
+		double v_truth = robot_pose.twist.twist.linear.x;
+		double omega_truth = robot_pose.twist.twist.angular.z;
+		if (v_truth != 0 || omega_truth != 0)
+		{
+			for (size_t i = 1; i < particle_num+1; i++)
+			{
+				double v_noise = v_truth + distribution_linear_velocity(generator);
+				double omega_noise = omega_truth + distribution_angular_velocity(generator);
+				nav_msgs::Odometry particle;
+				g_robot[i].to_msg(particle);
+				particle.twist.twist.linear.x = v_noise;
+				particle.twist.twist.angular.z = omega_noise;
+				g_robot[i].set_msg(particle);
+				g_robot[i].update();
+				particles_msg.poses[i-1] = particle.pose.pose;
+			}
+		}
+
+		pub_odom.publish(robot_pose);
+		pub_particles.publish(particles_msg);
+        
+		ros::spinOnce();
+		rate.sleep();
+		
+	}
+
 	return 0;
 }
