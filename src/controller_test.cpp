6c3458ef27aa8b4b329204bc2278adf964f7e488
yus-ko <yus-ko>
2024-03-26 09:50:04 +0900

    main

--- src/controller_test.cpp
+++ src/controller_test.cpp
@@ -1,15 +1,20 @@
 #include <ros/ros.h>
 #include <potbot_lib/DiffDriveController.h>
 #include <dynamic_reconfigure/server.h>
-#include <controller_tutorial/controller_tutorialConfig.h>
+#include <controller_tutorial/controller_testConfig.h>
 #include <geometry_msgs/PoseArray.h>
 
 potbot_lib::Controller::DiffDriveController g_robot;
 ros::Publisher g_pub_cmd;
+bool g_subscribed_goal = false;
 
 void odom_callback(const nav_msgs::Odometry& msg)
 {
 	g_robot.set_msg(msg);
+	if (!g_subscribed_goal)
+		g_robot.set_target(	msg.pose.pose.position.x,
+							msg.pose.pose.position.y,
+							potbot_lib::utility::get_Yaw(msg.pose.pose.orientation));
 	g_robot.pid_control();
 	nav_msgs::Odometry robot_pose;
 	g_robot.to_msg(robot_pose);
@@ -17,7 +22,15 @@ void odom_callback(const nav_msgs::Odometry& msg)
 	g_pub_cmd.publish(robot_pose.twist.twist);
 }
 
-void param_callback(const controller_tutorial::controller_tutorialConfig& param, uint32_t level)
+void goal_callback(const geometry_msgs::PoseStamped& msg)
+{
+	g_subscribed_goal = true;
+	g_robot.set_target(	msg.pose.position.x,
+						msg.pose.position.y,
+						potbot_lib::utility::get_Yaw(msg.pose.orientation));
+}
+
+void param_callback(const controller_tutorial::controller_testConfig& param, uint32_t level)
 {
 	g_robot.set_gain(	param.gain_p,
 							param.gain_i,
@@ -31,31 +44,26 @@ void param_callback(const controller_tutorial::controller_tutorialConfig& param,
 }
 
 int main(int argc,char **argv){
-	ros::init(argc,argv,"controller_tutorial");
+	ros::init(argc,argv,"controller_test");
 
 	ros::NodeHandle n("~");
 
-	double control_frequency = 50.0;
-	int particle_num = 100;
-	double norm_noise_mean_linear_velocity = 0;
-	double norm_noise_variance_linear_velocity = 0.1;
-	double norm_noise_mean_angular_velocity = 0;
-	double norm_noise_variance_angular_velocity = 0.1;
-	
-	n.getParam("control_frequency", control_frequency);
-	n.getParam("particle_num", particle_num);
-	n.getParam("norm_noise_mean_linear_velocity", norm_noise_mean_linear_velocity);
-	n.getParam("norm_noise_variance_linear_velocity", norm_noise_variance_linear_velocity);
-	n.getParam("norm_noise_mean_angular_velocity", norm_noise_mean_angular_velocity);
-	n.getParam("norm_noise_variance_angular_velocity", norm_noise_variance_angular_velocity);
+	std::string topic_cmd = "cmd_vel";
+	std::string topic_odom = "odom";
+	std::string topic_goal = "move_base_simple/goal";
+
+	n.getParam("topic_cmd", topic_cmd);
+	n.getParam("topic_odom", topic_odom);
+	n.getParam("topic_goal", topic_goal);
 
 	ros::NodeHandle nh;
 
-	g_pub_cmd					= nh.advertise<geometry_msgs::Twist>("/robot_5/cmd_vel", 1);
-	ros::Subscriber sub_odom				= nh.subscribe("/robot_5/odom",1,odom_callback);
+	g_pub_cmd					= nh.advertise<geometry_msgs::Twist>(topic_cmd, 1);
+	ros::Subscriber sub_odom				= nh.subscribe(topic_odom,1,odom_callback);
+	ros::Subscriber sub_goal				= nh.subscribe(topic_goal,1,goal_callback);
 
-	dynamic_reconfigure::Server<controller_tutorial::controller_tutorialConfig> server;
-	dynamic_reconfigure::Server<controller_tutorial::controller_tutorialConfig>::CallbackType f;
+	dynamic_reconfigure::Server<controller_tutorial::controller_testConfig> server;
+	dynamic_reconfigure::Server<controller_tutorial::controller_testConfig>::CallbackType f;
 	f = boost::bind(param_callback, _1, _2);
 	server.setCallback(f);
 	
