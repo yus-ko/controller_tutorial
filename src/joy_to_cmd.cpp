8eb3712c7085a4640acf1246226e431660ab14ea
yus-ko <koike3691@gmail.com>
2024-04-16 18:15:44 +0900

    add elecom pad

--- src/joy_to_cmd.cpp
+++ src/joy_to_cmd.cpp
@@ -5,30 +5,82 @@
 ros::Publisher g_pub_cmd;
 double g_max_linear_velocity = 0.5;
 double g_max_angular_velocity = 1.0;
+std::string g_game_pad_name = "xbox_one_controller";
 
 void joy_callback(const sensor_msgs::Joy& msg)
 {
+	double left_stick_x			= 0.0;
+	double left_stick_y			= 0.0;
+	double right_stick_x		= 0.0;
+	double right_stick_y		= 0.0;
+	double left_triger			= 1.0;
+	double right_triger			= 1.0;
+	double d_pad_x				= 0.0;
+	double d_pad_y				= 0.0;
 
-	double left_stick_x = msg.axes[0];	//左 正方向 1~-1
-	double left_stick_y = msg.axes[1];	//上 正方向 1~-1
-	double right_stick_x = msg.axes[3];
-	double right_stick_y = msg.axes[4];
-	double left_triger = msg.axes[2];
-	double right_triger = msg.axes[5];
-	double d_pad_x = msg.axes[6];	//左 正方向 1~-1
-	double d_pad_y = msg.axes[7];	//上 正方向 1~-1
+	bool button_a				= false;
+	bool button_b				= false;
+	bool button_x				= false;
+	bool button_y				= false;
+	bool button_left			= false;
+	bool button_right			= false;
+	bool button_select			= false;
+	bool button_start			= false;
+	bool button_home			= false;
+	bool button_left_stick		= false;
+	bool button_right_stick		= false;
 
-	bool button_a = msg.buttons[0];
-	bool button_b = msg.buttons[1];
-	bool button_x = msg.buttons[2];
-	bool button_y = msg.buttons[3];
-	bool button_left = msg.buttons[4];
-	bool button_right = msg.buttons[5];
-	bool button_select = msg.buttons[6];
-	bool button_start = msg.buttons[7];
-	bool button_home = msg.buttons[8];
-	bool button_left_stick = msg.buttons[9];
-	bool button_right_stick = msg.buttons[10];
+	if (g_game_pad_name == "xbox_one_controller")
+	{
+		left_stick_x		= msg.axes[0];	//左 正方向 1~-1
+		left_stick_y		= msg.axes[1];	//上 正方向 1~-1
+		right_stick_x		= msg.axes[3];
+		right_stick_y		= msg.axes[4];
+		left_triger			= msg.axes[2];
+		right_triger		= msg.axes[5];
+		d_pad_x				= msg.axes[6];	//左 正方向 1~-1
+		d_pad_y				= msg.axes[7];	//上 正方向 1~-1
+
+		button_a			= msg.buttons[0];
+		button_b			= msg.buttons[1];
+		button_x			= msg.buttons[2];
+		button_y			= msg.buttons[3];
+		button_left			= msg.buttons[4];
+		button_right		= msg.buttons[5];
+		button_select		= msg.buttons[6];
+		button_start		= msg.buttons[7];
+		button_home			= msg.buttons[8];
+		button_left_stick	= msg.buttons[9];
+		button_right_stick	= msg.buttons[10];
+	}
+	else if (g_game_pad_name == "elecom_JC-U3912T")
+	{
+		left_stick_x		= msg.axes[0];	//左 正方向 1~-1
+		left_stick_y		= msg.axes[1];	//上 正方向 1~-1
+		right_stick_x		= msg.axes[4];
+		right_stick_y		= msg.axes[3];
+		d_pad_x				= msg.axes[4];	//左 正方向 1~-1
+		d_pad_y				= msg.axes[5];	//上 正方向 1~-1
+
+		if (msg.buttons[4]) left_triger = -1.0;
+		else 				left_triger = 1.0;
+
+		if (msg.buttons[5]) right_triger = -1.0;
+		else 				right_triger = 1.0;
+
+		button_a			= msg.buttons[2];
+		button_b			= msg.buttons[3];
+		button_x			= msg.buttons[0];
+		button_y			= msg.buttons[1];
+		button_left			= msg.buttons[6];
+		button_right		= msg.buttons[7];
+		button_select		= msg.buttons[10];
+		button_start		= msg.buttons[11];
+		button_left_stick	= msg.buttons[8];
+		button_right_stick	= msg.buttons[9];
+	}
+	
+	
 
 	double max_linear_velocity = 0.5;
 	double max_angular_velocity = 1.0;
@@ -72,6 +124,7 @@ int main(int argc,char **argv){
 	n.getParam("topic_joy",					topic_joy);
 	n.getParam("max_linear_velocity",		g_max_linear_velocity);
 	n.getParam("max_angular_velocity",		g_max_angular_velocity);
+	n.getParam("game_pad_name",				g_game_pad_name);
 
 
 	ros::NodeHandle nh;
