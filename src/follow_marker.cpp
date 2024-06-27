4a5b684ffe95895d382ed2088f99629d3dc1de4d
yus-ko <koike3691@gmail.com>
2024-06-27 17:36:59 +0900

    pluginlib

--- src/follow_marker.cpp
+++ src/follow_marker.cpp
@@ -1,24 +1,26 @@
+#include <potbot_lib/pure_pursuit.h>
 #include <potbot_lib/diff_drive_controller_ros.h>
 #include <potbot_lib/interactive_marker_manager.h>
+#include <pluginlib/class_loader.h>
 
 class FollowMarker
 {
-private:
-	ros::Publisher pub_cmd_;
-	ros::Subscriber sub_odom_;
-	potbot_lib::controller::DiffDriveControllerROS* ddr_;
-	potbot_lib::InteractiveMarkerManager* ims_;
+	private:
+		ros::Publisher pub_cmd_;
+		ros::Subscriber sub_odom_;
+		boost::shared_ptr<potbot_lib::controller::BaseController> ddr_;
+		potbot_lib::InteractiveMarkerManager* ims_;
 
-	void odomCallback(const nav_msgs::Odometry& msg);
+		void odomCallback(const nav_msgs::Odometry& msg);
 
-public:
-	FollowMarker(potbot_lib::controller::DiffDriveControllerROS* ddr, potbot_lib::InteractiveMarkerManager *imm);
-	~FollowMarker(){};
+	public:
+		FollowMarker(potbot_lib::InteractiveMarkerManager* imm);
+		~FollowMarker(){};
 };
 
-FollowMarker::FollowMarker(potbot_lib::controller::DiffDriveControllerROS* ddr, potbot_lib::InteractiveMarkerManager *imm)
+FollowMarker::FollowMarker(potbot_lib::InteractiveMarkerManager* imm)
 {
-	ddr_ = ddr;
+
 	ims_ = imm;
 
 	visualization_msgs::Marker marker_target;
@@ -34,63 +36,43 @@ FollowMarker::FollowMarker(potbot_lib::controller::DiffDriveControllerROS* ddr, 
 	ims_->initInteractiveMarkers(marker_target);
 	ims_->initInteractiveMarkerServer();
 
-	// ros::NodeHandle n("~");
+	ros::NodeHandle n("~");
+	
+	pluginlib::ClassLoader<potbot_lib::controller::BaseController> loader("potbot_lib", "potbot_lib::controller::BaseController");
+	std::string plugin_name = "potbot_lib/PurePursuit";
+	n.getParam("controller_name", plugin_name);
+	try
+	{
+		ddr_ = loader.createInstance(plugin_name);
+		ddr_->initialize("controller");
+	}
+	catch(pluginlib::PluginlibException& ex)
+	{
+		ROS_ERROR("failed to load plugin. Error: %s", ex.what());
+	}
 
 	ros::NodeHandle nh;
-
 	pub_cmd_					= nh.advertise<geometry_msgs::Twist>("cmd_vel", 1);
-
 	sub_odom_					= nh.subscribe("odom",1, &FollowMarker::odomCallback, this);
-
-	// dynamic_reconfigure::Server<controller_tutorial::controller_testConfig>::CallbackType f;
-	// f = boost::bind(&FollowMarker::__param_callback, this, _1, _2);
-	// server_.setCallback(f);
-
-	// static tf2_ros::TransformListener tfListener(tf_buffer_);
 }
 
 void FollowMarker::odomCallback(const nav_msgs::Odometry& msg)
 {
 	const geometry_msgs::Point target = ims_->getMarker()->front().pose.position;
-	// ROS_INFO_STREAM(target);
-
 	ddr_->setTargetPath(ims_->getMarkerTrajectories()->front());
-	ddr_->setMsg(msg);
-	ddr_->purePursuit();
-	ddr_->publishLookahead();
+	ddr_->setRobot(msg);
+	geometry_msgs::Twist cmd;
+	ddr_->calculateCommand(cmd);
 
-	nav_msgs::Odometry robot;
-	ddr_->toMsg(robot);
-	
-	// double x = msg.pose.pose.position.x;
-	// double y = msg.pose.pose.position.y;
-	// double yaw = tf2::getYaw(msg.pose.pose.orientation);
-
-	// double l_d = hypot(target.y-y,target.x-x);
-
-	// geometry_msgs::Twist cmd;
-	// if (l_d > 0.2)
-	// {
-	// 	double alpha = atan2(target.y-y,target.x-x) - yaw;
-	// 	double v = 0.2;
-		
-	// 	double omega = 2.0*v*sin(alpha)/l_d;
-
-		
-	// 	cmd.linear.x = v;
-	// 	cmd.angular.z = omega;
-	// }
-
-	pub_cmd_.publish(robot.twist.twist);
-	
+	pub_cmd_.publish(cmd);
 }
 
 int main(int argc,char **argv){
 	ros::init(argc,argv,"marker_controller");
 
 	potbot_lib::InteractiveMarkerManager imm("target");
-	potbot_lib::controller::DiffDriveControllerROS ddr("controller");
-	FollowMarker fmc(&ddr, &imm);
+	FollowMarker fmc(&imm);
+
 	ros::spin();
 
 	return 0;
